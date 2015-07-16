d56a040ed4c3525827914fba16740173bb357d85
Dablu Kumar <dabluk@thoughtworks.com>
2015-07-16 11:22:33 +0530

    Proper statement added for taking inputs for the mars rover

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -11,14 +11,20 @@ public class Main {
     public static void main(String args[]){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
+            System.out.println("Enter the Plateau X position");
             int plateauX = Integer.parseInt(br.readLine());
+            System.out.println("Enter the Plateau Y position");
             int plateauY = Integer.parseInt(br.readLine());
             System.out.println("How many input you want");
             int number = Integer.parseInt(br.readLine());
             while (number != 0) {
+                System.out.println("Enter the position X");
                 int positionX = Integer.parseInt(br.readLine());
+                System.out.println("Enter the position Y");
                 int positionY = Integer.parseInt(br.readLine());
+                System.out.println("Enter the direction");
                 String direction = br.readLine();
+                System.out.println("Enter the input for moving the rover");
                 String input = br.readLine();
                 MarsRover marsRover = new MarsRover(positionX, positionY, plateauX, plateauY, direction);
                 marsRover.moveInPlateau(input);
